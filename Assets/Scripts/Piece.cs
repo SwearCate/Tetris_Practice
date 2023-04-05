36f68a9c9ca1677b50986bde1eac0503766c7a9c
SwearCate <94557386+SwearCate@users.noreply.github.com>
2023-04-05 15:50:07 +0300

    Movement + bounds

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -24,5 +24,41 @@ public class Piece : MonoBehaviour
         }
 
     }
+
+    private void Update()
+    {
+        this.board.Clear(this);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+
+        this.board.Set(this);
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.isValidPosition(this, newPosition);
+
+        if (valid)
+        {
+            this.position = newPosition;
+        }
+
+        return valid;
+    }
 }
 
